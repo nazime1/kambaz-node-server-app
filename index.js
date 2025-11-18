2b63a39b546d48845ffdea05a9facbf347450634
Siggy Nazimowitz <thewotter@icloud.com>
2025-11-18 15:22:36 -0500

    uudghdugdbgdhugd

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
  cors({
    credentials: true,
-   origin: process.env.CLIENT_URL || "http://localhost:3000",
+   origin: * || "http://localhost:3000",
  })
 );
 const sess = {
