5b5502755eddb5968631566b22946e1736b92595
Siggy Nazimowitz <thewotter@icloud.com>
2025-11-30 12:39:37 -0500

    oops

--- index.js
+++ index.js
@@ -15,14 +15,18 @@ app.use(cors({
    credentials: true,
    origin: process.env.CLIENT_URL || "http://localhost:3000"
 }));
-const sess = {
+const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
   resave: false,
   saveUninitialized: false,
 }
-if (app.get('env') === 'production') {
-  app.set('trust proxy', 1) // trust first proxy
-  sess.cookie.secure = true // serve secure cookies
+if (process.env.SERVER_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.SERVER_URL,
+  };
 }
 app.use(session(sess));
 app.use(express.json());
